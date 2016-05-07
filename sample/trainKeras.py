2074a098ce78a8a3f68396d376a265d4223fbc32
wailoktam <wailoktam@gmail.com>
2016-05-07 14:16:42 +0900

    try removing [] from b4 lambda

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -96,7 +96,7 @@ def make_network():
 
    mergedKerasModel = Sequential()
 #   mergedKerasModel.add(Merge([leftKerasModel,rightKerasModel], mode= lambda l, r: dot(l,r.T)/linalg.norm(l).linalg.norm(r)))
-   mergedKerasModel.add(Merge([leftKerasModel, rightKerasModel], mode=lambda x, y: x - y))
+   mergedKerasModel.add(Merge(leftKerasModel, rightKerasModel, mode=lambda x, y: x - y))
    mergedKerasModel.add(Dense(10),activation='softmax')
    return mergedKerasModel
 
