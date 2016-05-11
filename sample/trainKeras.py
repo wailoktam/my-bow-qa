51a17fc4dd4d81a3e4f78c0472feb1a8f90b4142
wailoktam <wailoktam@gmail.com>
2016-05-11 17:44:14 +0900

    change obj fct

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -120,7 +120,7 @@ def train_test_model(km, testData, labels):
    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
    print ("train input shape")
    print km.input_shape
-   km.compile(loss='custom_objective', optimizer=sgd)
+   km.compile(loss='hinge', optimizer=sgd)
    km.fit(testData, labels, nb_epoch=1, batch_size=32)
 
 
