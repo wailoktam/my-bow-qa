b28cb397e48fced36037b8227e4093ec899555fb
wailoktam <wailoktam@gmail.com>
2016-05-17 13:29:43 +0900

    change Model to .add

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -124,7 +124,7 @@ def make_test_network():
    rightKerasModel.add(Activation('relu'))
    rightKerasModel.add(MaxPooling2D(pool_size=(2, 2)))
 
-#   mergedKerasModel = Sequential()
+   mergedKerasModel = Sequential()
 #   mergedKerasModel.add(Merge([leftKerasModel,rightKerasModel], mode= lambda l, r: dot(l,r.T)/linalg.norm(l).linalg.norm(r)))
 #   merged = Merge([leftKerasModel, rightKerasModel], mode=lambda x: x[0]*x[1]/linalg.norm(x[0]).linalg.norm(x[1]))
 #   merged = Merge([leftKerasModel, rightKerasModel], mode='cos', output_shape=(10, 50,50))
@@ -133,8 +133,8 @@ def make_test_network():
    cos_distance = Merge([leftKerasModel, rightKerasModel], mode='cos', dot_axes=1) # magic dot_axes works here!
    cos_distance = Reshape((1,))(cos_distance)
    cos_similarity = Lambda(lambda x: 1-x)(cos_distance)
-
-   mergedKerasModel = Model([leftKerasModel, rightKerasModel], [cos_similarity])
+   mergedKerasModel.add(cos_similarity)
+#   mergedKerasModel = Model([leftKerasModel, rightKerasModel], [cos_similarity])
 #   mergedKerasModel.add(merged)
 #   mergedKerasModel.add(Lambda(lambda x: 1 - x))
 #   mergedKerasModel.add(Flatten())
