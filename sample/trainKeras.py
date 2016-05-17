92a4b646b0bb796b6297c82524a508247401005b
wailoktam <wailoktam@gmail.com>
2016-05-17 13:18:33 +0900

    change cos 2 sample

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -127,7 +127,7 @@ def make_test_network():
 #   mergedKerasModel = Sequential()
 #   mergedKerasModel.add(Merge([leftKerasModel,rightKerasModel], mode= lambda l, r: dot(l,r.T)/linalg.norm(l).linalg.norm(r)))
 #   merged = Merge([leftKerasModel, rightKerasModel], mode=lambda x: x[0]*x[1]/linalg.norm(x[0]).linalg.norm(x[1]))
-   merged = Merge([leftKerasModel, rightKerasModel], mode='cos', output_shape=(10, 50,50))
+#   merged = Merge([leftKerasModel, rightKerasModel], mode='cos', output_shape=(10, 50,50))
 #   merged = Merge([leftKerasModel, rightKerasModel], mode=lambda x: x[0] - x[1], output_shape=(10,50,50))
 
    cos_distance = Merge([leftKerasModel, rightKerasModel], mode='cos', dot_axes=1) # magic dot_axes works here!
