1f73918aaf12980da00d8ba92fe99eb5d00c2f8f
wailoktam <wailoktam@gmail.com>
2016-05-26 13:24:35 +0900

    change label to testlabel

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
    mergeLayer.add(Merge([leftKerasModel, rightKerasModel], mode='cos', dot_axes=1))
    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
    mergeLayer.compile(loss='custom_objective', optimizer='sgd')
-   mergeLayer.fit([test3dLArray, test3dRArray], labels, nb_epoch=10, batch_size=32)
+   mergeLayer.fit([test3dLArray, test3dRArray], testLabels, nb_epoch=10, batch_size=32)
    result = mergeLayer.predict([test3dLArray, test3dRArray], verbose=1)
 
    print result
