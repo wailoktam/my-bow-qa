b058b4bdab87136c476192ea4675ac802ef8fa45
wailoktam <wailoktam@gmail.com>
2016-05-26 18:01:08 +0900

    change layer name

--- sample/trainKeras.py
+++ sample/trainKeras.py
@@ -389,7 +389,7 @@ if __name__ == '__main__':
 
    rightKerasModel.add(Reshape((1, 200)))
    convolution1d_2 = Convolution1D(10, 3, border_mode='same')
-   rightKerasModel.add(convolution1d_1)
+   rightKerasModel.add(convolution1d_2)
    rightKerasModel.add(Activation('relu'))
  #  rightKerasModel.add(MaxPooling1D(pool_length=2, stride=None, border_mode='valid'))
  #  rightKerasModel.add(Flatten())
